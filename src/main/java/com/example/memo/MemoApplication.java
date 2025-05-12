b501c531f500502c36e53872c899509f2ea0ef53
Geyapse <8290m@naver.com>
2025-05-12 11:32:22 +0900

    메모장 실습테스트

--- src/main/java/com/example/memo/MemoApplication.java
+++ src/main/java/com/example/memo/MemoApplication.java
@@ -2,7 +2,7 @@ package com.example.memo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//메모장실습
+//메모장실습2
 @SpringBootApplication
 public class MemoApplication {
 
